03be6534a02b751d523138e0800a766494c49d8c
cedric <cedricnettler@gmail.com>
2022-12-15 15:34:29 +0100

    adds minimal Project

--- src/lagerhalterungsystem.cc
+++ src/lagerhalterungsystem.cc
@@ -12,21 +12,32 @@ int main(int argc, char** argv){
 
     CLI::App app{"Schreibe: -r src/lagerhalterung.json \n"};
 
-    std::string filepath;
+    std::string filepath, inhalt, zaehlen, aendern;
 
-    app.add_option("-r,--read", filepath,"Path to config file")
+    //Irgendwie die options als "starter" für for(...) benennen? 
+    app.add_option("-i,--inhalt", inhalt, "Zeug"); //Inhalt ausgeben lassen
+
+    app.add_option("-z,--zählen", zaehlen, "mehr Zeug"); //Inhalt zählen lassen
+
+    app.add_option("-s,--ändern", aendern, "ganz viel Zeug"); //Inhalt ändern und in neuer Datei Speichern
+
+
+    app.add_option("-r,--read", filepath, "Path to config file")
         ->required()
         ->check(CLI::ExistingFile);
 
-    try{
+    try
+    {
         app.parse(argc, argv);
     }
-    catch(const CLI::ParseError &e){
+    catch(const CLI::ParseError &e)
+    {
         return app.exit(e);
     }
 
     std::ifstream file{filepath};
-    if(!file.is_open()){
+    if(!file.is_open())
+    {
         std::cout << "Error opening file!\n" << std::endl;
         exit(0);
     }
@@ -42,25 +53,47 @@ int main(int argc, char** argv){
         std::cerr << "parse error at byte " << ex.byte << std::endl;
     }
 
-    for (auto& element : database_object["Regale"]){
-    
+    //Inhalt der Regale wird ausgegeben
+    for (auto& element : database_object["Regale"])
+    {
         std::cout << "Das Regal " << element["Regal"] << " beinhaltet folgende dinge: " << element["Inhalt"] << std::endl;
-        std::cout << "\033[1;31mAllgemein: \033[0m" << "Es sind " << element["Leere Plätze"] << " von " << element["Anzahl Lagerplätze"] << " Plätze frei \n" << std::endl;
+        std::cout << "Es sind " << element["Leere Plätze"] << " von " << element["Anzahl Lagerplätze"] << " Plätze frei" << "\033[1;31m (Ausgebenen)\033[0m\n" << std::endl;
     }
 
-    /**********************************/
-    std::cout << "\033[1;31mPlatzhalter\n\033[0m" << std::endl;
+    /*
+    //Freie Plätze sollen gezählt werden, anhand Stückgut des Inhaltes (1)
+    for (auto zaehlen = database_object["Inhalt"].begin(); zaehlen != database_object["Inhalt"].end(); ++zaehlen)
+    {
+        int num = stoi(zaehlen["Inhalt"]);
+
+        std::cout << "Es sind " << *zaehlen << " Plätze frei" <<  "\033[1;31mRechnerisch: \033[0m\n" << std::endl;
+    }
+    */
     
-    for (auto& element : database_object["Regale: Inhalt"]){
+    /*
+    //Freie Plätze sollen gezählt werden, anhand Stückgut des Inhaltes (2)
+    for (auto count = database_object("Inhalt"))
+    {
+        std::cout << "Es sind " << count << " Plätze frei" << "\033[1;31mRechnerisch: \033[0m\n" << std::endl;
+    }
+    */
 
-        std::cout <<  "\033[1;31mRechnerisch: \033[0m\n" << "Es sind " << element["Fernseher"] << element["Radio"] << element["SAT-Anlage"] << " von " << element["Anzahl Lagerplätze"] << " Plätze frei \n" << std::endl;
+    //Ändern des Inhaltes der eingelesenen Datei
+    for (auto& change : database_object["Regale"])
+    {
+        std::cout << "Möchten Sie den Inhalt: " <<change["Inhalt"] << " ändern?" << " Neuer Name: "  << std::endl; 
+        std::cin >> change["Inhalt"];
+        std::cout << "\nDer geänderte Inhalt heißt: " << change["Inhalt"] << "\n" << std::endl;
     }
 
-    for (auto& element : database_object["Inhalt"]){
-
-        std::cout <<  "\033[1;31mRechnerisch: \033[0m\n" << "Es sind " << element["Fernseher"] << element["Radio"] << element["SAT-Anlage"] << " von " << element["Anzahl Lagerplätze"] << " Plätze frei \n" << std::endl;
-    }
-    /**********************************/
+    //Speichern des Inhaltes der geänderten, eingelesenen Datei
+    std::string speicherpfad{};
+    std::cout << "Name der Datei: ";
+    std::cin >> speicherpfad;
+    std::cout << "\nSpeicherpfad: /CLI11/" << speicherpfad << std::endl;
+    std::ofstream save_as{speicherpfad};
+    save_as << database_object.dump();
+    save_as.close();
 
     return 0;
 }
